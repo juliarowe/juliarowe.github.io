24e485b43c49ac1553c03a754a5c860adec34887
Julia Rowe <Julia@Julias-MacBook-Pro.local>
2015-08-17 09:30:28 -0400

    moved files

---
+++ old/global.js
@@ -0,0 +1,6 @@
+//global variables
+wWidth = $(window).width();
+wHeight = $(window).height();
+
+menuItems = ['about me', 'research', 'photography', 'graphic design', 'resume', 'contact'];
+/*socialItems = []*/
