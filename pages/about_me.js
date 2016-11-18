6002b2a91e9793e936f3666867250120224b5406
juliarowe <juliarowe@users.noreply.github.com>
2016-11-18 11:52:25 -0500

    page files to go with new functionality

---
+++ pages/about_me.js
@@ -0,0 +1,58 @@
+class aboutMePage {
+	
+	get html() {
+		var html =	
+		"<div id='about-me-menu'>\
+			<ul class='about-me-nav'>\
+				<li id='about-me-nc' class='about-me-nav-item' data-target='about-me-north_carolina'>\
+					<img class='about-me-menu-pic' src='images/North Carolina.png'/>\
+					<div class = 'overlay'></div>\
+					<p class='about-me-menu-pic-txt'>NORTH CAROLINA</p>\
+				</li>\
+				<li id='about-me-da' class='about-me-nav-item' data-target='about-me-durham_academy'>\
+					<img class='about-me-menu-pic' src='images/Durham Academy.png'/>\
+					<div class = 'overlay'></div>\
+					<p class='about-me-menu-pic-txt'>DURHAM ACADEMY</p>\
+				</li>\
+				<li id='about-me-tu' class='about-me-nav-item' data-target='about-me-tufts_university'>\
+					<img class='about-me-menu-pic' src='images/Tufts University.png'/>\
+					<div class = 'overlay'></div>\
+					<p class='about-me-menu-pic-txt'>TUFTS UNIVERSITY</p>\
+				</li>\
+				<li id='about-me-trav' class='about-me-nav-item' data-target='about-me-travel'>\
+					<img class='about-me-menu-pic' src='images/Travel.png'/>\
+					<div class = 'overlay'></div>\
+					<p class='about-me-menu-pic-txt'>TRAVEL</p>\
+				</li>\
+			</ul>\
+		</div>\
+		<div id='about-me-content'>\
+			<div id='about-me-north_carolina' class='about-me-section' data-par='about-me-nc'>\
+				<div class='about-me-title '> NORTH CAROLINA </div>\
+				<div class='about-me-text'>\
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium ut nibh vitae congue. In ut urna sit amet diam dapibus molestie vel hendrerit sapien. Nam et velit odio. Sed facilisis, purus ut varius hendrerit, velit ligula lobortis tellus, in porttitor erat nunc eget risus. Nullam facilisis nisl vel leo lacinia elementum. Etiam sit amet justo dolor. Maecenas pellentesque pretium turpis ac dignissim. Vestibulum aliquet dui vehicula felis scelerisque, id tempor quam congue. Aliquam laoreet, felis ac ultricies interdum, sapien est dapibus lacus, ac rutrum ipsum risus ut quam. Sed tortor ligula, vulputate non porttitor nec, scelerisque eget sem. Aenean ac diam vel augue placerat efficitur a eget dui. Integer in dolor quis enim gravida dictum. Nam sit amet quam a nulla lobortis ultrices vitae ut turpis. <br> Nullam sit amet auctor massa. Cras id luctus ipsum, eu dapibus arcu. Donec ac sem volutpat felis cursus placerat ut quis tellus. Aliquam pulvinar, orci ac ultrices rhoncus, mi enim semper elit, nec porta odio lectus ut velit. Integer nisi mi, sollicitudin dignissim maximus at, ultricies sagittis tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam placerat lorem et interdum placerat. Suspendisse et accumsan justo. Quisque feugiat justo felis, ac gravida lectus iaculis ac. <br> Praesent vitae elit efficitur, consectetur tortor sit amet, vulputate sem. In feugiat, ligula non lobortis luctus, ante est pulvinar sem, nec porttitor dolor magna ac velit. Maecenas a purus sed purus tempus auctor. Fusce bibendum dui in lorem pharetra consequat. Fusce porttitor euismod volutpat. Nulla mollis nunc nisi. Vivamus ut sodales ante, vestibulum suscipit augue. Morbi justo dui, molestie sit amet leo non, viverra feugiat metus. Vestibulum eget lectus rhoncus orci placerat aliquam eget vel tortor. Donec vel ex ipsum.\
+				</div>\
+			</div>\
+			<div id='about-me-durham_academy' class='about-me-section' data-par='about-me-da'>\
+				<div class='about-me-title '> DURHAM ACADEMY </div>\
+				<div class='about-me-text'>\
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium ut nibh vitae congue. In ut urna sit amet diam dapibus molestie vel hendrerit sapien. Nam et velit odio. Sed facilisis, purus ut varius hendrerit, velit ligula lobortis tellus, in porttitor erat nunc eget risus. Nullam facilisis nisl vel leo lacinia elementum. Etiam sit amet justo dolor. Maecenas pellentesque pretium turpis ac dignissim. Vestibulum aliquet dui vehicula felis scelerisque, id tempor quam congue. Aliquam laoreet, felis ac ultricies interdum, sapien est dapibus lacus, ac rutrum ipsum risus ut quam. Sed tortor ligula, vulputate non porttitor nec, scelerisque eget sem. Aenean ac diam vel augue placerat efficitur a eget dui. Integer in dolor quis enim gravida dictum. Nam sit amet quam a nulla lobortis ultrices vitae ut turpis. <br> Nullam sit amet auctor massa. Cras id luctus ipsum, eu dapibus arcu. Donec ac sem volutpat felis cursus placerat ut quis tellus. Aliquam pulvinar, orci ac ultrices rhoncus, mi enim semper elit, nec porta odio lectus ut velit. Integer nisi mi, sollicitudin dignissim maximus at, ultricies sagittis tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam placerat lorem et interdum placerat. Suspendisse et accumsan justo. Quisque feugiat justo felis, ac gravida lectus iaculis ac. <br> Praesent vitae elit efficitur, consectetur tortor sit amet, vulputate sem. In feugiat, ligula non lobortis luctus, ante est pulvinar sem, nec porttitor dolor magna ac velit. Maecenas a purus sed purus tempus auctor. Fusce bibendum dui in lorem pharetra consequat. Fusce porttitor euismod volutpat. Nulla mollis nunc nisi. Vivamus ut sodales ante, vestibulum suscipit augue. Morbi justo dui, molestie sit amet leo non, viverra feugiat metus. Vestibulum eget lectus rhoncus orci placerat aliquam eget vel tortor. Donec vel ex ipsum.\
+				</div>\
+			</div>\
+			<div id='about-me-tufts_university' class='about-me-section' data-par='about-me-tu'>\
+				<div class='about-me-title '> TUFTS UNIVERSITY </div>\
+				<div class='about-me-text'>\
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium ut nibh vitae congue. In ut urna sit amet diam dapibus molestie vel hendrerit sapien. Nam et velit odio. Sed facilisis, purus ut varius hendrerit, velit ligula lobortis tellus, in porttitor erat nunc eget risus. Nullam facilisis nisl vel leo lacinia elementum. Etiam sit amet justo dolor. Maecenas pellentesque pretium turpis ac dignissim. Vestibulum aliquet dui vehicula felis scelerisque, id tempor quam congue. Aliquam laoreet, felis ac ultricies interdum, sapien est dapibus lacus, ac rutrum ipsum risus ut quam. Sed tortor ligula, vulputate non porttitor nec, scelerisque eget sem. Aenean ac diam vel augue placerat efficitur a eget dui. Integer in dolor quis enim gravida dictum. Nam sit amet quam a nulla lobortis ultrices vitae ut turpis. <br> Nullam sit amet auctor massa. Cras id luctus ipsum, eu dapibus arcu. Donec ac sem volutpat felis cursus placerat ut quis tellus. Aliquam pulvinar, orci ac ultrices rhoncus, mi enim semper elit, nec porta odio lectus ut velit. Integer nisi mi, sollicitudin dignissim maximus at, ultricies sagittis tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam placerat lorem et interdum placerat. Suspendisse et accumsan justo. Quisque feugiat justo felis, ac gravida lectus iaculis ac. \
+				</div>\
+			</div>\
+			<div id='about-me-travel' class='about-me-section' data-par='about-me-trav'>\
+				<div class='about-me-title '> TRAVEL </div>\
+				<div class='about-me-text'>			\
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium ut nibh vitae congue. In ut urna sit amet diam dapibus molestie vel hendrerit sapien. Nam et velit odio. Sed facilisis, purus ut varius hendrerit, velit ligula lobortis tellus, in porttitor erat nunc eget risus. Nullam facilisis nisl vel leo lacinia elementum. Etiam sit amet justo dolor. Maecenas pellentesque pretium turpis ac dignissim. Vestibulum aliquet dui vehicula felis scelerisque, id tempor quam congue. Aliquam laoreet, felis ac ultricies interdum, sapien est dapibus lacus, ac rutrum ipsum risus ut quam. Sed tortor ligula, vulputate non porttitor nec, scelerisque eget sem. Aenean ac diam vel augue placerat efficitur a eget dui. Integer in dolor quis enim gravida dictum. Nam sit amet quam a nulla lobortis ultrices vitae ut turpis. <br> Nullam sit amet auctor massa. Cras id luctus ipsum, eu dapibus arcu. Donec ac sem volutpat felis cursus placerat ut quis tellus. Aliquam pulvinar, orci ac ultrices rhoncus, mi enim semper elit, nec porta odio lectus ut velit. Integer nisi mi, sollicitudin dignissim maximus at, ultricies sagittis tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam placerat lorem et interdum placerat. Suspendisse et accumsan justo. Quisque feugiat justo felis, ac gravida lectus iaculis ac. <br> Praesent vitae elit efficitur, consectetur tortor sit amet, vulputate sem. In feugiat, ligula non lobortis luctus, ante est pulvinar sem, nec porttitor dolor magna ac velit. Maecenas a purus sed purus tempus auctor. Fusce bibendum dui in lorem pharetra consequat. Fusce porttitor euismod volutpat. Nulla mollis nunc nisi. Vivamus ut sodales ante, vestibulum suscipit augue. Morbi justo dui, molestie sit amet leo non, viverra feugiat metus. Vestibulum eget lectus rhoncus orci placerat aliquam eget vel tortor. Donec vel ex ipsum. <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium ut nibh vitae congue. In ut urna sit amet diam dapibus molestie vel hendrerit sapien. Nam et velit odio. Sed facilisis, purus ut varius hendrerit, velit ligula lobortis tellus, in porttitor erat nunc eget risus. Nullam facilisis nisl vel leo lacinia elementum. Etiam sit amet justo dolor. Maecenas pellentesque pretium turpis ac dignissim. Vestibulum aliquet dui vehicula felis scelerisque, id tempor quam congue. Aliquam laoreet, felis ac ultricies interdum, sapien est dapibus lacus, ac rutrum ipsum risus ut quam. Sed tortor ligula, vulputate non porttitor nec, scelerisque eget sem. Aenean ac diam vel augue placerat efficitur a eget dui. Integer in dolor quis enim gravida dictum. Nam sit amet quam a nulla lobortis ultrices vitae ut turpis. <br> Nullam sit amet auctor massa. Cras id luctus ipsum, eu dapibus arcu. Donec ac sem volutpat felis cursus placerat ut quis tellus. Aliquam pulvinar, orci ac ultrices rhoncus, mi enim semper elit, nec porta odio lectus ut velit. Integer nisi mi, sollicitudin dignissim maximus at, ultricies sagittis tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam placerat lorem et interdum placerat. Suspendisse et accumsan justo. Quisque feugiat justo felis, ac gravida lectus iaculis ac. <br> Praesent vitae elit efficitur, consectetur tortor sit amet, vulputate sem. In feugiat, ligula non lobortis luctus, ante est pulvinar sem, nec porttitor dolor magna ac velit. Maecenas a purus sed purus tempus auctor. Fusce bibendum dui in lorem pharetra consequat. Fusce porttitor euismod volutpat. Nulla mollis nunc nisi. Vivamus ut sodales ante, vestibulum suscipit augue. Morbi justo dui, molestie sit amet leo non, viverra feugiat metus. Vestibulum eget lectus rhoncus orci placerat aliquam eget vel tortor. Donec vel ex ipsum.\
+				</div>\
+			</div>\
+		</div>";
+
+		return html;
+	}
+}
