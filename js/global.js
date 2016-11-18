9755ce677f91ed93633849691a068bd0435a390c
juliarowe <juliarowe@users.noreply.github.com>
2016-11-18 11:49:06 -0500

    added page object

--- js/global.js
+++ js/global.js
@@ -2,7 +2,7 @@
 var wWidth = $(window).width();
 var wHeight = $(window).height();
 
-var menuItems = ['about me', 'projects', 'graphic design', 'photography','resume', 'contact'];
+var menuItems = ['about Me', 'projects', 'graphic Design', 'photography','resume', 'contact'];
 
 var socialItems = [{'name':'github', 'URL':'http://github.com/juliarowe'}, 
 				   {'name':'linkedin', 'URL':'https://www.linkedin.com/in/juliabrowe'},
@@ -10,4 +10,6 @@ var socialItems = [{'name':'github', 'URL':'http://github.com/juliarowe'},
 				   {'name':'tumblr', 'URL':'http://fullofquarks.tumblr.com'}, 
 				   {'name':'twitter', 'URL':'https://twitter.com/J_R0WE'}];
 
-var funcObj = {}
+var funcObj = {};
+
+var pages = {};
