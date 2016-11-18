99996813cb8fc7a2b33dc66ce2530073ba18d46f
juliarowe <juliarowe@users.noreply.github.com>
2016-11-18 11:48:35 -0500

    got page loading without doing a refresh.
    updated UI to work with that.  Fixed footer.  fixed menu displaying.  Added resume and contact functionality.

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -6,70 +6,76 @@
 //find backgrounds
 
 
-$('body').ready(function()
-{
-	updateUI();
-	setListeners();
-});
+class aboutMe {
 
+	get loadPage() {
+		var html = pages.aboutMe.html;
+		$('#content')[0].innerHTML = html;
+		this.setListeners();
+		this.updateUI();
+	}
 
-function setListeners()
-{
-	$('.nav-item').on('click', function() 
-	{
-		var target = '#' + $(this).data('target');
-		var scrollDest = $(target).position().top;
-		$('body').animate({scrollTop:scrollDest}, 100);
-	});
+	setListeners() {
+		$('.about-me-nav-item').on('click', function() {
+			var target = '#' + $(this).data('target');
+			var scrollDest = $(target).position().top + 20;
+			$('body').animate({scrollTop:scrollDest}, 100);
+		});
 
-	$(window).scroll(function()
-	{
-		var scrollContent = $.find('.about-me-section');
-		var scrollNav = $.find('.nav-item');
-		var scrolledAbove = scrollContent.map(function(elem)
-			{
-				var fromTop = $(this).scrollTop()+96;
-    			if ($(elem).offset().top < fromTop)
-    				return elem;	
-			});
-		
-		var cur;
-		for (var i in scrolledAbove){
-			if (scrolledAbove[i] != undefined)
-				cur = scrolledAbove[i];
-		};
+		$(window).scroll(function() {
+			var scrollContent = $.find('.about-me-section');
+			var scrollNav = $.find('.about-me-nav-item');
+			var scrolledAbove = scrollContent.map(function(elem) {
+					var fromTop = $(window).scrollTop() + 169;
+	    			if ($(elem).offset().top < fromTop)
+	    				return elem;	
+				});
+			
+			var cur;
+			for (var i in scrolledAbove){
+				if (scrolledAbove[i] != undefined)
+					cur = scrolledAbove[i];
+			};
 
-	    var curNav = '#' + $(cur).data('par');
+		    var curNav = '#' + $(cur).data('par');
 
-	    if (!$(curNav).hasClass('active')) {
-	    	scrollNav.map(function(elem)
-	    	{
-	    		if ($(elem).hasClass('active'))
-	    			$(elem).removeClass('active');
-	   				$(elem).children().map(function(el, item) 
-	    			{
-	    				$(item).removeClass('active');
-	    			});
+		    if (!$(curNav).hasClass('active')) {
+		    	scrollNav.map(function(elem)
+		    	{
+		    		if ($(elem).hasClass('active'))
+		    			$(elem).removeClass('active');
+		   				$(elem).children().map(function(el, item) 
+		    			{
+		    				$(item).removeClass('active');
+		    			});
 
-	    	});
-	    	$(curNav).addClass('active');
-	    	$(curNav).children().map(function(el, item) 
-	   		{
-	   			$(item).addClass('active');
-	   		});
-	    }
-	});
-}
+		    	});
+		    	$(curNav).addClass('active');
+		    	$(curNav).children().map(function(el, item) 
+		   		{
+		   			$(item).addClass('active');
+		   		});
+		    }
+		});
 
-function updateUI()
-{
-	var text_arr = $('.menu-pic-txt');
-	var len = text_arr.length
-	var img_height = $('.menu-pic').height();
+		$(window).resize(function() {
+			var content_height = $('#about-me-content').height() + $('#about-me-content').offset().top - $('#content').css('padding-top').replace('px', '');
+			$('#content').css({height:content_height});
+		});
+	}
 
-	for (var i = 0; i < len; i++) {
-		var text_height = $($('.menu-pic-txt')[i]).height();
-		var text_top = (img_height - text_height) / 2;
-		$($('.menu-pic-txt')[i]).css({'top':(text_top + 'px')});
+	updateUI() {
+		var text_arr = $('.about-me-menu-pic-txt');
+		var len = text_arr.length
+		var img_height = $('.about-me-menu-pic').height();
+
+		for (var i = 0; i < len; i++) {
+			var text_height = $($('.about-me-menu-pic-txt')[i]).height();
+			var text_top = (img_height - text_height) / 2;
+			$($('.about-me-menu-pic-txt')[i]).css({'top':(text_top + 'px')});
+		}
+
+		var content_height = $('#about-me-content').height() + $('#about-me-content').offset().top - $('#content').css('padding-top').replace('px', '');
+		$('#content').css({height:content_height});
 	}
 }
